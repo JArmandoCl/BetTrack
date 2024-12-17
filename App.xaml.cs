2339d25153d70b5c40a2eb4e68bfc1f39b36c34b
José Armando Cárdenas Lara <armando.cardenas@sw.massoluciones.com>
2024-12-16 20:47:51 -0600

    Primer commit de la app sin pagina principal y logo definido

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BetTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BetTrack
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
